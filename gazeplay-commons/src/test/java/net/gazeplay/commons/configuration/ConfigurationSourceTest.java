0fd4107738ea0d413f4128e17660eb537ecca114
PillouMatou <vincent.armone@etu.univ-grenoble-alpes.fr>
2022-01-25 11:24:58 +0100

    release test 4 spotbug try to fix

--- gazeplay-commons/src/test/java/net/gazeplay/commons/configuration/ConfigurationSourceTest.java
+++ gazeplay-commons/src/test/java/net/gazeplay/commons/configuration/ConfigurationSourceTest.java
@@ -21,6 +21,13 @@ class ConfigurationSourceTest {
     private final File resourcesFolder = new File(localDataFolder);
 
     @Test
+    void shouldCreateFromPropertiesResource() {
+        Configuration result = ConfigurationSource.createFromPropertiesResource(new File(resourcesFolder, "GazePlay.properties"));
+
+        assertEquals("eng", result.getLanguage());
+    }
+
+    @Test
     void givenPropertiesFileNotFound_shouldCreateDefaultProperties() {
         Configuration result = ConfigurationSource.createFromPropertiesResource(new File(localDataFolder, "wrong.properties"));
 
